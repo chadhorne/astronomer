6277fd83170c38ffde3457901e1f1d74b50e70d2
Chad Horne <w35628@watcocompanies.com>
2022-03-24 16:45:44 -0500

    simple_dag example

---
+++ dags/simple_dag.py
@@ -0,0 +1,59 @@
+from multiprocessing.connection import wait
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from airflow.models.baseoperator import chain, cross_downstream
+
+from datetime import datetime, timedelta
+
+default_args = {
+    'retry': 5,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'email': 'chad.horne@watco.com'
+}
+
+def _downloading_data(ti, **kwargs):
+    with open('/tmp/my_file.txt', 'w') as f:
+        f.write('my_data')
+    ti.xcom_push(key='my_key', value=43)
+    return 42
+
+def _checking_data(ti):
+    my_xcom = ti.xcom_pull(key='my_key', task_ids=['downloading_data'])
+    print(my_xcom)
+
+def _failure(context):
+    print("On callback failure")
+    print(context)
+
+with DAG(dag_id='simple_dag', default_args=default_args, schedule_interval="@daily",
+    start_date=days_ago(3), catchup=True) as dag:
+    
+    downloading_data = PythonOperator(
+        task_id='downloading_data',
+        python_callable=_downloading_data
+    )
+
+    checking_data = PythonOperator(
+        task_id='checking_data',
+        python_callable=_checking_data
+    )
+
+    waiting_for_data = FileSensor(
+        task_id='waiting_for_data',
+        fs_conn_id='fs_default',
+        filepath='my_file.txt'
+    )
+
+    processing_data = BashOperator(
+        task_id='processing_data',
+        bash_command='exit 1',
+        on_failure_callback=_failure
+    )
+
+    chain(downloading_data, checking_data, waiting_for_data, processing_data)
